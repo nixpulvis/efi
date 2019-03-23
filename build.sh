746ecb6cd408925c58b6cb9be7ec768299b66cb0
Nathan Lilienthal <nathan@nixpulvis.com>
2019-03-23 17:28:23 -0400

    Small refactor, update for needed `-P` flag, and no root.

--- build.sh
+++ build.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
-
 set -xe
 
 TARGET=hello.img
 MOUNT=mnt
 
-gcc hello.c -c -fno-stack-protector -fpic -fshort-wchar -mno-red-zone -I /usr/include/efi/ -I /usr/include/efi/x86_64/ -DEFI_FUNCTION_WRAPPER -o hello.o
-ld hello.o /usr/lib/crt0-efi-x86_64.o -nostdlib -znocombreloc -T /usr/lib/elf_x86_64_efi.lds -shared -Bsymbolic -L /usr/lib -l:libgnuefi.a -l:libefi.a -o hello.so
-objcopy -j .text -j .sdata -j .data -j .dynamic -j .dynsym -j .rel -j .rela -j .reloc --target=efi-app-x86_64 hello.so hello.efi
+gcc hello.c \
+    -c -fno-stack-protector -fpic -fshort-wchar -mno-red-zone \
+    -I /usr/include/efi/ \
+    -I /usr/include/efi/x86_64/ \
+    -DEFI_FUNCTION_WRAPPER \
+    -o hello.o
+
+ld hello.o \
+    /usr/lib/crt0-efi-x86_64.o -nostdlib -znocombreloc \
+    -T /usr/lib/elf_x86_64_efi.lds \
+    -shared -Bsymbolic \
+    -L /usr/lib -l:libgnuefi.a -l:libefi.a \
+    -o hello.so
+
+objcopy \
+    -j .text -j .sdata -j .data -j .dynamic \
+    -j .dynsym -j .rel -j .rela -j .reloc \
+    --target=efi-app-x86_64 hello.so hello.efi
 
 # Create an image with enough space for the EFI executable.
 # TODO: Can we create an image with exactly the right size easily?
@@ -28,22 +42,20 @@ n
 w
 EOF
 
-losetup /dev/loop0 $TARGET
+sudo losetup -P /dev/loop0 $TARGET
 
 # Format the image.
-mkfs.vfat -F32 /dev/loop0p1
+sudo mkfs.vfat -F32 /dev/loop0p1
 
 # Create a mountpoint to mount the image through a loopback interface.
-mkdir -p $MOUNT 
-mount /dev/loop0p1 $MOUNT
+mkdir -p $MOUNT
+sudo mount /dev/loop0p1 $MOUNT
 
-bootctl install --path $MOUNT
-ls -la $MOUNT
+sudo bootctl install --path $MOUNT
 
-exit 0
-
+exit
 # Clean up the mount, our image should be ready.
-umount $MOUNT
-losetup -d /dev/loop0
+sudo umount $MOUNT
+sudo losetup -d /dev/loop0
 rm -r $MOUNT
 
